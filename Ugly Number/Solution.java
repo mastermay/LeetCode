072fbd03d6bfc1a9f91a313ef57dc97a9c8d7ab4
mastermay <986492616@qq.com>
2015-12-19 22:17:14 +0800

    update

---
+++ Ugly Number/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+	public boolean isUgly(int num) {
+		if (num < 1) {
+			return false;
+		}
+		if (num == 1) {
+			return true;
+		}
+		while (num % 2 == 0) {
+			num /= 2;
+		}
+		while (num % 3 == 0) {
+			num /= 3;
+		}
+		while (num % 5 == 0) {
+			num /= 5;
+		}
+		return num == 1 ? true : false;
+	}
+}
---
+++ Ugly Number/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(new Solution().isUgly(12));
+		System.out.println(new Solution().isUgly(16));
+		System.out.println(new Solution().isUgly(14));
+	}
+}
